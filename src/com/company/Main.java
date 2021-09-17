538f9c62cfe5066faf7de65fa56abc1531a73e8f
unknown <khanhsan03@gmail.com>
2021-09-17 07:48:52 +0700

    tinh tong tien lai

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double money = 1.0;
+	int month = 1;
+	double interestRate = 1.0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Nhập số tiền gửi : ");
+        money = input.nextDouble();
+
+        System.out.println("Nhập số tháng gửi : ");
+        month = input.nextInt();
+
+        System.out.println("Nhập lãi suất: ");
+        interestRate = input.nextDouble();
+
+        double totalInterest = 0 ;
+        for(int i = 0; i < month; i++){
+            totalInterest += money * (interestRate/100)/12 * month;
+        }
+        System.out.println("lai suat cua ban : "+ totalInterest);
+
+    }
+}
